5fe3a56301e91e55c86dd9c1c4561c755ed640c2
James Croft <james.r.croft@gmail.com>
2013-08-07 21:34:39 +0100

    The API only allows one listing_id per request

--- lib/zoopla/listings/listing.rb
+++ lib/zoopla/listings/listing.rb
@@ -2,7 +2,7 @@ class Zoopla
 
   # Common methods for Sales and Rentals classes
   module Listing
-                
+
     # Defines the search area. All possible params are described at http://developer.zoopla.com/docs/
     # @param [Hash] Location hash
     # @return [Sales, Rentals]
@@ -11,23 +11,23 @@ class Zoopla
       @request.merge! location
       self
     end
-    
+
     # Sets the price range
     # @param [Range] price range (weekly in case of rentals), e.g. 200..300
     # @return [Sales, Rentals]
     def price(price_range)
       set_range_parameter(:price, price_range)
     end
-    
+
     alias_method :for, :price
-    
+
     # Sets the number of bedrooms range
     # @param [Range] bedrooms range, e.g. 2..3
     # @return [Sales, Rentals]
     def beds(beds_range)
       set_range_parameter(:beds, beds_range)
     end
-    
+
     # Defines the size of the search area in miles. E.g. 2 miles around a location
     # @param [Fixum] radius in miles
     # @return [Sales, Rentals]
@@ -36,18 +36,18 @@ class Zoopla
       @request[:radius] = miles
       self
     end
-    
+
     alias_method :radius, :within
-    
+
     # The field by which the results should be ordered, either :price or :age of listing.
     # @param [Symbol, String] sorting field
     # @return [Sales, Rentals]
     def order_by(field)
       ensure_valid_parameter('sorting order', field, %w(price age))
-      @request[:order_by] = field.to_s        
+      @request[:order_by] = field.to_s
       self
     end
-    
+
     # Sort order for the listings returned. Either :descending or :ascending
     # @param [Symbol, String] sorting order
     # @return [Sales, Rentals]
@@ -56,7 +56,7 @@ class Zoopla
       @request[:ordering] = order.to_s
       self
     end
-    
+
     # Property type. Either :houses or :flats
     # @param [Symbol, String] property type
     # @return [Sales, Rentals]
@@ -65,21 +65,21 @@ class Zoopla
       @request[:property_type] = type.to_s
       self
     end
-    
+
     # Search only for houses
     # @return [Sales, Rentals]
     def houses
       @request[:property_type] = 'houses'
       self
     end
-    
+
     # Search only for flats
     # @return [Sales, Rentals]
     def flats
       @request[:property_type] = 'flats'
       self
     end
-    
+
     # Keywords to search for within the listing description.
     # @param [Array, String] keywords
     # @return [Sales, Rentals]
@@ -89,46 +89,45 @@ class Zoopla
       @request[:keywords] = keywords
       self
     end
-    
+
     # Sets the minimum price. Weekly prices in case of rentals
     # @param [Fixnum] price
     # @return [Sales, Rentals]
     def minimum_price(price)
       set_limiting_value(:minimum, :price, price)
     end
-    
+
     # Sets the maximum price. Weekly prices in case of rentals
     # @param [Fixnum] price
     # @return [Sales, Rentals]
     def maximum_price(price)
       set_limiting_value(:maximum, :price, price)
     end
-    
+
     # Sets the minimum number of bedrooms
     # @param [Fixnum] number of bedrooms
     # @return [Sales, Rentals]
     def minimum_beds(beds)
       set_limiting_value(:minimum, :beds, beds)
     end
-    
+
     # Sets the maximum number of bedrooms
     # @param [Fixnum] number of bedrooms
     # @return [Sales, Rentals]
     def maximum_beds(beds)
       set_limiting_value(:maximum, :beds, beds)
     end
-    
+
     # Adds another listing id to the query. Calling this function multiple times will add multiple ids.
     # Please note that other provided arguments will still be taken into account when filtering listings
     # @param [Fixnum] listing id
     # @return [Sales, Rentals]
     def listing_id(listing_id)
       ensure_valid_parameter('listing id', listing_id, lambda {|k| k.is_a? Fixnum and k >= 0})
-      @request[:listing_id] ||= []
-      @request[:listing_id] << listing_id
-      self        
+      @request[:listing_id] = listing_id
+      self
     end
-    
+
     # Iterates over the results. Possible fields are described at http://developer.zoopla.com/docs/read/Property_listings
     # @yield [Hashie::Mash] a listing with data, e.g. sales.each{|listing| puts listing.price }
     # @raise Zoopla::DisambiguationError if an ambiguous area is specified. Always expect this exception
@@ -143,40 +142,40 @@ class Zoopla
         listings.each do |listing|
           yield listing
         end
-      end while fetched_so_far < number_of_results 
+      end while fetched_so_far < number_of_results
     end
-        
+
     private
-    
+
     def set_limiting_value(limit, attribute, value)
       ensure_valid_parameter("#{limit} #{attribute}", value, lambda {|p| p.is_a? Fixnum and p >= 0})
       @request["#{limit}_#{attribute}".to_sym] = value
-      self        
+      self
     end
-    
+
     def set_range_parameter(attribute, value)
       ensure_valid_parameter(attribute.to_s, value, lambda {|pr| pr.is_a? Fixnum or (pr.is_a?(Range) and pr.first <= pr.last)})
       value = value..value if value.is_a? Fixnum
       self.send("minimum_#{attribute}", value.first)
       self.send("maximum_#{attribute}", value.last)
-      self        
+      self
     end
-    
+
     def ensure_valid_parameter(parameter, value, valid_options)
       raise "#{parameter} not specified" unless value
       raise "Unknown #{parameter}: #{value}" if valid_options.is_a? Array and !valid_options.include?(value.to_s)
       raise "Invalid #{parameter}: #{value}" if valid_options.is_a? Proc  and !valid_options.call(value)
     end
-    
+
     def preprocess(reply)
       number_of_results = reply.result_count || 0
       [number_of_results, reply.listing]
     end
-    
+
     def api_call
       'property_listings'
-    end      
-    
-  end  
+    end
+
+  end
 
 end
--- test/test_listings.rb
+++ test/test_listings.rb
@@ -1,144 +1,144 @@
 require 'helper'
 
 class TestZooplaListings < Test::Unit::TestCase
-  
+
   def setup
     zoopla = Zoopla.new('my_api_key')
     @rentals = zoopla.rentals
     @sales   = zoopla.sales
   end
-  
+
   def test_reset_for_sales
     @sales.include_sold
     assert_equal({:listing_status=>"sale", :include_sold => '1'}, @sales.instance_variable_get('@request'))
     @sales.reset!
     assert_equal({:listing_status=>"sale"}, @sales.instance_variable_get('@request'))
   end
-  
+
   def test_reset_for_rentals
     @rentals.include_rented
     assert_equal({:listing_status=>"rent", :include_rented => '1'}, @rentals.instance_variable_get('@request'))
     @rentals.reset!
     assert_equal({:listing_status=>"rent"}, @rentals.instance_variable_get('@request'))
   end
-  
+
   def test_in
     listing_parameter_test(:in, {:postcode => 'E1W 3TJ'}, {:postcode => 'E1W 3TJ'})
   end
-  
+
   def test_within
-    listing_parameter_test(:within, 2, {:radius => 2})    
+    listing_parameter_test(:within, 2, {:radius => 2})
   end
-  
+
   def test_radius
     listing_parameter_test(:radius, 2, {:radius => 2})
   end
-  
+
   def test_order_by
     listing_parameter_test(:order_by, :age, {:order_by => 'age'})
     listing_parameter_test(:order_by, :price, {:order_by => 'price'})
     assert_raise RuntimeError do
-      listing_parameter_test(:order_by, :blah, {}) 
+      listing_parameter_test(:order_by, :blah, {})
     end
     assert_raise RuntimeError do
-      listing_parameter_test(:order_by, nil, {}) 
+      listing_parameter_test(:order_by, nil, {})
     end
   end
-  
+
   def test_ordering
     listing_parameter_test(:ordering, :ascending, {:ordering => 'ascending'})
     listing_parameter_test(:ordering, :descending, {:ordering => 'descending'})
     assert_raise RuntimeError do
-      listing_parameter_test(:ordering, :blah, {}) 
-    end    
+      listing_parameter_test(:ordering, :blah, {})
+    end
     assert_raise RuntimeError do
-      listing_parameter_test(:ordering, nil, {}) 
-    end    
+      listing_parameter_test(:ordering, nil, {})
+    end
   end
-  
+
   def test_include_sold
     @sales.include_sold
     expected = {:listing_status=>"sale", :include_sold => '1'}
-    assert_equal expected, @sales.instance_variable_get('@request')    
+    assert_equal expected, @sales.instance_variable_get('@request')
   end
-  
+
   def test_include_rented
     @rentals.include_rented
     expected = {:listing_status=>"rent", :include_rented => '1'}
-    assert_equal expected, @rentals.instance_variable_get('@request')        
+    assert_equal expected, @rentals.instance_variable_get('@request')
   end
-  
+
   def test_minimum_price
     listing_parameter_test(:minimum_price, 300, {:minimum_price => 300})
     assert_raise RuntimeError do
-      listing_parameter_test(:minimum_price, nil, {}) 
-    end    
+      listing_parameter_test(:minimum_price, nil, {})
+    end
     assert_raise RuntimeError do
-      listing_parameter_test(:minimum_price, -200, {}) 
-    end        
+      listing_parameter_test(:minimum_price, -200, {})
+    end
   end
-  
+
   def test_maximum_price
     listing_parameter_test(:maximum_price, 500, {:maximum_price => 500})
     assert_raise RuntimeError do
-      listing_parameter_test(:maximum_price, nil, {}) 
-    end    
+      listing_parameter_test(:maximum_price, nil, {})
+    end
     assert_raise RuntimeError do
-      listing_parameter_test(:maximum_price, -200, {}) 
-    end        
+      listing_parameter_test(:maximum_price, -200, {})
+    end
   end
-  
+
   def test_price
     listing_parameter_test(:for, 200..500, {:minimum_price => 200, :maximum_price => 500})
     listing_parameter_test(:price, 200..500, {:minimum_price => 200, :maximum_price => 500})
     listing_parameter_test(:price, 200..200, {:minimum_price => 200, :maximum_price => 200})
     listing_parameter_test(:price, 200, {:minimum_price => 200, :maximum_price => 200})
     assert_raise RuntimeError do
-      listing_parameter_test(:price, -200, {}) 
-    end            
+      listing_parameter_test(:price, -200, {})
+    end
     assert_raise RuntimeError do
-      listing_parameter_test(:price, 500..200, {}) 
-    end            
+      listing_parameter_test(:price, 500..200, {})
+    end
     assert_raise RuntimeError do
-      listing_parameter_test(:price, nil, {}) 
-    end            
-  end  
-  
+      listing_parameter_test(:price, nil, {})
+    end
+  end
+
   def test_minimum_beds
     listing_parameter_test(:minimum_beds, 3, {:minimum_beds => 3})
     assert_raise RuntimeError do
-      listing_parameter_test(:minimum_beds, nil, {}) 
-    end    
+      listing_parameter_test(:minimum_beds, nil, {})
+    end
     assert_raise RuntimeError do
-      listing_parameter_test(:minimum_beds, -2, {}) 
-    end        
+      listing_parameter_test(:minimum_beds, -2, {})
+    end
   end
-  
+
   def test_maximum_beds
     listing_parameter_test(:maximum_beds, 5, {:maximum_beds => 5})
     assert_raise RuntimeError do
-      listing_parameter_test(:maximum_beds, nil, {}) 
-    end    
+      listing_parameter_test(:maximum_beds, nil, {})
+    end
     assert_raise RuntimeError do
-      listing_parameter_test(:maximum_beds, -2, {}) 
-    end        
+      listing_parameter_test(:maximum_beds, -2, {})
+    end
   end
-  
+
   def test_beds
     listing_parameter_test(:beds, 2..5, {:minimum_beds => 2, :maximum_beds => 5})
     listing_parameter_test(:beds, 2..2, {:minimum_beds => 2, :maximum_beds => 2})
     listing_parameter_test(:beds, 2, {:minimum_beds => 2, :maximum_beds => 2})
     assert_raise RuntimeError do
-      listing_parameter_test(:beds, -2, {}) 
-    end            
+      listing_parameter_test(:beds, -2, {})
+    end
     assert_raise RuntimeError do
-      listing_parameter_test(:beds, 5..2, {}) 
-    end            
+      listing_parameter_test(:beds, 5..2, {})
+    end
     assert_raise RuntimeError do
-      listing_parameter_test(:beds, nil, {}) 
-    end            
-  end  
-  
+      listing_parameter_test(:beds, nil, {})
+    end
+  end
+
   def test_furnished
     listing_parameter_test(:furnished, 'furnished', {:furnished => 'furnished'})
     listing_parameter_test(:furnished, 'furnished', {:furnished => 'furnished'})
@@ -151,40 +151,38 @@ class TestZooplaListings < Test::Unit::TestCase
       listing_parameter_test(:furnished, nil, {})
     end
   end
-  
+
   def test_property_type
     listing_parameter_test(:property_type, 'houses', {:property_type => 'houses'})
     listing_parameter_test(:property_type, 'flats', {:property_type => 'flats'})
     assert_raise RuntimeError do
       listing_parameter_test(:property_type, nil, {})
-    end    
+    end
     assert_raise RuntimeError do
       listing_parameter_test(:property_type, 'igloos', {})
-    end    
+    end
   end
-  
+
   def test_houses
     @sales.houses
     expected = {:listing_status=>"sale", :property_type => 'houses'}
-    assert_equal expected, @sales.instance_variable_get('@request')    
+    assert_equal expected, @sales.instance_variable_get('@request')
   end
-  
+
   def test_flats
     @sales.flats
     expected = {:listing_status=>"sale", :property_type => 'flats'}
-    assert_equal expected, @sales.instance_variable_get('@request')    
+    assert_equal expected, @sales.instance_variable_get('@request')
   end
-  
+
   def test_keywords
     listing_parameter_test(:keywords, 'very cheap centrally located spacious modern duplex', {:keywords => 'very cheap centrally located spacious modern duplex'})
   end
-  
+
   def test_listing_id
-    listing_parameter_test(:listing_id, 7, {:listing_id => [7]})
-    listing_parameter_test(:listing_id, 11, {:listing_id => [7, 11]})
-    listing_parameter_test(:listing_id, 13, {:listing_id => [7, 11, 13]})
+    listing_parameter_test(:listing_id, 7, {:listing_id => 7})
   end
-  
+
   def test_forming_the_request_by_chaining
     @rentals.flats.in({:postcode => 'E1W 3TJ'}).within(2).price(200..400).beds(1..2).furnished('furnished').include_rented.order_by(:price).ordering(:descending)
     expected = { :listing_status=>"rent",
@@ -201,19 +199,19 @@ class TestZooplaListings < Test::Unit::TestCase
                  :ordering=>"descending"}
     assert_equal expected, @rentals.instance_variable_get('@request')
   end
-  
+
   def test_only_correct_listing_types_requested_for_sales
     @sales.in({:postcode => 'E1W 3TJ'})
     expected = {:listing_status=>"sale", :postcode => 'E1W 3TJ'}
-    assert_equal expected, @sales.instance_variable_get('@request')    
+    assert_equal expected, @sales.instance_variable_get('@request')
   end
-  
+
   def test_only_correct_listing_types_requested_for_rentals
     @rentals.in({:postcode => 'E1W 3TJ'})
     expected = {:listing_status=>"rent", :postcode => 'E1W 3TJ'}
-    assert_equal expected, @rentals.instance_variable_get('@request')    
+    assert_equal expected, @rentals.instance_variable_get('@request')
   end
-  
+
   def test_requesting_multiple_results_pages_transparently
     mock = mock();
     mock.expects(:process_listing).times(12);
@@ -222,9 +220,9 @@ class TestZooplaListings < Test::Unit::TestCase
     @rentals.stubs(:fetch_data).returns(page1, page2)
     @rentals.in({:postcode => 'E1W 3TJ'}).within(0.1).price(300..400).each {|listing|
       mock.process_listing
-    }    
+    }
   end
-  
+
   def test_api_returns_wrong_result_count
     @rentals.stubs(:call).returns([200, api_reply('wrong_result_count')], [200, api_reply('wrong_result_count2')])
     yield_count = 0
@@ -233,13 +231,13 @@ class TestZooplaListings < Test::Unit::TestCase
     }
     assert_equal 8, yield_count
   end
-  
+
   private
-  
+
   def listing_parameter_test(param, value, result)
     @rentals.send(param, value)
     expected = {:listing_status=>"rent"}.merge! result
-    assert_equal expected, @rentals.instance_variable_get('@request')        
+    assert_equal expected, @rentals.instance_variable_get('@request')
   end
-  
+
 end
